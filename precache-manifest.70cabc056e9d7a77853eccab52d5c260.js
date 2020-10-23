46f90c8a446b68270f5c4a6994385a8e71c4b42a
Ksyunomorph <49567382+viyatsun@users.noreply.github.com>
2020-10-23 14:07:13 +0300

    Updates

---
+++ precache-manifest.70cabc056e9d7a77853eccab52d5c260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12be4e8d526ca95c3ccbc4eff2193d4",
+    "url": "/Movies/index.html"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/Movies/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "fde5407091de19ef98ad",
+    "url": "/Movies/static/css/main.cab8b1b3.chunk.css"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/Movies/static/js/2.6481e93c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Movies/static/js/2.6481e93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde5407091de19ef98ad",
+    "url": "/Movies/static/js/main.5311feca.chunk.js"
+  },
+  {
+    "revision": "cc1777ea6604cf61dfa8",
+    "url": "/Movies/static/js/runtime-main.32f615eb.js"
+  }
+]);
